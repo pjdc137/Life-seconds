d59704b03a48fdb553ad9751cc47bd04ac34c4d1
pjdc137 <51933887+pjdc137@users.noreply.github.com>
2026-01-04 23:13:30 -0500

    update

--- app.py
+++ app.py
@@ -33,4 +33,8 @@ def calculate():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
